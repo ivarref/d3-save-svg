5409fa2b021a8b7851047530b592cb432c6a54d1
edeno <edeno@bu.edu>
2015-12-01 11:33:22 -0500

    Add some defaults for filename based on crowbar

--- src/d3-save-svg.js
+++ src/d3-save-svg.js
@@ -3,7 +3,21 @@ import preprocess from './preprocess';
 
 export function save(svgElement, config) {
   var defaultFileName = 'test';
+  var defaultFileName = getDefaultFileName(svgInfo);
   var filename = config.filename || defaultFileName;
   var svgInfo = preprocess(svgElement);
   download(svgInfo, filename);
+}
+
+function getDefaultFileName(svgInfo) {
+  var defaultFileName = 'untitled';
+  if (svgInfo.id) {
+    defaultFileName = svgInfo.id;
+  } else if (svgInfo.class) {
+    defaultFileName = svgInfo.class;
+  } else if (window.document.title) {
+    defaultFileName = window.document.title.replace(/[^a-z0-9]/gi, '-').toLowerCase();
+  }
+
+  return defaultFileName;
 }
