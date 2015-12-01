faa6b073a62a88d71bfba40bc9eb39ad3e5ae374
edeno <edeno@bu.edu>
2015-12-01 11:34:28 -0500

    Minor fix

--- src/d3-save-svg.js
+++ src/d3-save-svg.js
@@ -2,10 +2,10 @@ import download from './download';
 import preprocess from './preprocess';
 
 export function save(svgElement, config) {
-  var defaultFileName = 'test';
   if (svgElement.nodeName !== 'svg' || svgElement.nodeType !== 1) {
     throw 'Need an svg element input';
   }
+
   var config = config || {};
   var svgInfo = preprocess(svgElement, config);
   var defaultFileName = getDefaultFileName(svgInfo);
