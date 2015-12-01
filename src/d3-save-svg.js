b41109224eced459ea6cb1aa498eeca71d28279c
edeno <edeno@bu.edu>
2015-11-30 23:08:33 -0500

    Allow for SVG element and filename to be specified

--- src/d3-save-svg.js
+++ src/d3-save-svg.js
@@ -1,3 +1,9 @@
-export default function() {
-  return 42;
-};
+import download from './download';
+import preprocess from './preprocess';
+
+export function save(svgElement, config) {
+  var defaultFileName = 'test';
+  var filename = config.filename || defaultFileName;
+  var svgInfo = preprocess(svgElement);
+  download(svgInfo, filename);
+}
