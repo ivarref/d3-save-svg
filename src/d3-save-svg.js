5545086e0ee8d2ad27107005069c0c5cfa3cb28c
edeno <edeno@bu.edu>
2015-12-01 11:34:10 -0500

    Do some checking of arguments

--- src/d3-save-svg.js
+++ src/d3-save-svg.js
@@ -3,6 +3,11 @@ import preprocess from './preprocess';
 
 export function save(svgElement, config) {
   var defaultFileName = 'test';
+  if (svgElement.nodeName !== 'svg' || svgElement.nodeType !== 1) {
+    throw 'Need an svg element input';
+  }
+  var config = config || {};
+  var svgInfo = preprocess(svgElement, config);
   var defaultFileName = getDefaultFileName(svgInfo);
   var filename = config.filename || defaultFileName;
   var svgInfo = preprocess(svgElement);
